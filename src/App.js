6264dac1dd312467d304483d7a869064ecf55f50
lamaharmouch18 <52330808@students.liu.edu.lb>
2025-11-15 16:27:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-// REHAM
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
