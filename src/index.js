df44a423b0491902fc654f4fea42d376f49d638f
lamaharmouch18 <52330808@students.liu.edu.lb>
2025-11-22 21:42:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -17,31 +15,9 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-=======
-// REHAM
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 
 
 
->>>>>>> Stashed changes
-=======
-// REHAM
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 
 
 
->>>>>>> Stashed changes
