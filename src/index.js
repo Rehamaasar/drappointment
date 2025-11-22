4254cbcbb36ef7d7dae13808ad38f9c7f2e63141
Rehamaasar <52230205@students.liu.edu.lb>
2025-11-22 20:50:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -16,6 +17,20 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+=======
+// REHAM
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+
+>>>>>>> Stashed changes
 =======
 // REHAM
 
