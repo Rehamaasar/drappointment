04a30fd9332c4cd8d003c416957fb59dbe449e77
Rehamaasar <52230205@students.liu.edu.lb>
2025-11-22 14:42:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-// REHAM
-
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./App.css";
 import "./index.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 
-
-
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
