0d19c5fede4bf51afee86f13447dcc6c67e773af
Rehamaasar <52230205@students.liu.edu.lb>
2025-11-22 20:44:51 +0200

    Update Doctors.jsx

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // src/pages/Doctors.jsx
 
 import SectionHeader from "../components/SectionHeader.jsx";
@@ -20,6 +21,22 @@ function Doctors() {
             <DoctorCard key={doctor.id} doctor={doctor} />
           ))}
         </div>
+=======
+// REHAM
+
+import { doctors } from "../data/mockData";
+import DoctorCard from "../components/DoctorCard";
+
+function Doctors() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-6">Meet Our Doctors</h1>
+
+      <div className="grid grid-cols-3 gap-6">
+        {doctors.map((d) => (
+          <DoctorCard key={d.id} doctor={d} />
+        ))}
+>>>>>>> Stashed changes
       </div>
     </div>
   );
