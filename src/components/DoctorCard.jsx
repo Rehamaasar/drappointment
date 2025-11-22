4ca7700b485cde234d77bf601a472910f9acbddd
lamaharmouch18 <52330808@students.liu.edu.lb>
2025-11-22 22:03:56 +0200

    Update DoctorCard.jsx

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// src/components/DoctorCard.jsx
 import { Star } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
@@ -50,23 +47,8 @@ function DoctorCard({ doctor }) {
           View Profile
         </button>
       </div>
-=======
-=======
->>>>>>> Stashed changes
-// REHAM
-
-function DoctorCard({ doctor }) {
-  return (
-    <div className="bg-white shadow rounded-lg p-4">
-      <img src={doctor.image} className="w-full rounded-lg mb-4" />
-      <h3 className="text-xl font-bold">{doctor.name}</h3>
-      <p className="text-gray-600">{doctor.specialty}</p>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-    </div>
+      </div>
   );
 }
-
+  
 export default DoctorCard;
