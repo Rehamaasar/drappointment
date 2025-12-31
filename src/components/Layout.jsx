971bfcaabb8cbb8dc13f2dd937649c5b2abd5156
Rehamaasar <52230205@students.liu.edu.lb>
2026-01-01 01:19:42 +0200

    Final UI fixes

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+// Assigned to Lama
+// src/components/Layout.jsx
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-slate-50">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
+
