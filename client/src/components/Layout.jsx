ac490c52420f48d59dd66ff4c466a515d644ca5b
Rehamaasar <52230205@students.liu.edu.lb>
2026-01-01 11:27:12 +0200

    Update Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-function Layout({ children }) {
+function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-slate-50">
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
